15eb0eb435211f065141f67761e6e1d1030ebce4
Rob Miller <rdmagm062699@gmail.com>
2023-05-26 10:24:52 -0500

    Refactor quality calculation to a function

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,31 +1,12 @@
-def update_quality(items):
+from item import Item
+
+def update_quality(items: list[Item]):
     for item in items:
-        if not _aged_brie(item.name) and not _backstage_pass(item.name):
-            # TODO: Improve this code.  Word.
-            if item.quality > 0:
-                if not _sulfuras_hand_of_ragnaros(item.name):
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if _aged_brie(item.name):
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
-                # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                if _aged_brie(item.name):
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                if _backstage_pass(item.name):
-                    if item.sell_in < 11:
-                        # See revision number 2394 on SVN.
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+        item.quality = _calculate_quality(item)
+
         if not _sulfuras_hand_of_ragnaros(item.name):
             item.sell_in = item.sell_in - 1
+
         if item.sell_in < 0:
             if not _aged_brie(item.name):
                 if not _backstage_pass(item.name):
@@ -41,10 +22,42 @@ def update_quality(items):
                 if _aged_brie(item.name) and item.sell_in <= 0:
                     item.quality = 0
                     # of for.
+
         if not _sulfuras_hand_of_ragnaros(item.name):
             if item.quality > 50:
                 item.quality = 50
     return items
+
+
+def _calculate_quality(item: Item) -> int:
+    quality = item.quality
+
+    if not _aged_brie(item.name) and not _backstage_pass(item.name):
+        # TODO: Improve this code.  Word.
+        if quality > 0:
+            if not _sulfuras_hand_of_ragnaros(item.name):
+                quality = quality - 1
+    else:
+        if quality < 50:
+            quality = quality + 1
+            if _aged_brie(item.name):
+                if item.sell_in < 6:
+                    quality = quality + 1
+            # Increases the Quality of the stinky cheese if it's 11 days to due date.
+            if _aged_brie(item.name):
+                if item.sell_in < 11:
+                    quality = quality + 1
+            if _backstage_pass(item.name):
+                if item.sell_in < 11:
+                    # See revision number 2394 on SVN.
+                    if quality < 50:
+                        quality = quality + 1
+                # Increases the Quality of Backstage Passes if the Quality is 6 or less.
+                if item.sell_in < 6:
+                    if quality < 50:
+                        quality = quality + 1
+
+    return quality
 
 
 def _aged_brie(name: str):
