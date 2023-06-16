4c7b83644556067ed009ad710d5334fa223fc337
Kyle Adams <kadams54@users.noreply.github.com>
2023-06-16 11:14:53 -0400

    Lose more quality after sell-in
    Also switched the new sell-in to be calculated before the new quality.

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,8 +5,8 @@ class GildedRose:
     @staticmethod
     def update_quality(items):
         for item in items:
-            item.quality = update_quality(item)
             item.sell_in = update_sell_in(item)
+            item.quality = update_quality(item)
         return items
 
 
@@ -25,10 +25,16 @@ def calculate_appreciation(item: Item) -> int:
     return 1
 
 
+def calculate_depreciation(item: Item) -> int:
+    if item.sell_in < 0:
+        return 2
+    return 1
+
+
 def update_quality(item: Item) -> int:
     if appreciating_item(item):
         return item.quality + calculate_appreciation(item)
-    return item.quality - 1
+    return item.quality - calculate_depreciation(item)
 
 
 def update_sell_in(item: Item) -> int:
